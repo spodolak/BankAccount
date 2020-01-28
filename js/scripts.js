aefef6d33818238f666ab5f72f83e4550916fbd6
Student <example@example.com>
2020-01-28 14:22:36 -0800

    update scripts.js to store new account values and read starting balance as an integer

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,28 @@
-$("document").ready(function()  {
+//Back-End Logic
+function Bank() {
+  this.accounts = [];
+}
+
+function Account(name, amount) {
+  this.name = name;
+  this.amount = amount;
+}
+
+
+//Front-End Logic
+function showBalance(account) {
+  $("#balance").html(account.amount);
+}
+
+$("document").ready(function() {
+  $("form#new-account").submit(function() {
+    event.preventDefault();
+    var inputtedName = $("input#name").val();
+    var inputtedAmount = parseInt($("input#amount").val());
+    var account = new Account(inputtedName, inputtedAmount);
+    showBalance(account);
+
+  });
+
 
 });
