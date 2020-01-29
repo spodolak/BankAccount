2076bc32de59550542ce0a20b77c0ef5e7716b5f
Student <example@example.com>
2020-01-28 16:42:15 -0800

    update scripts.js to include user inputs for deposits and withdrawals, track unique ids for update account form and new balance form, calculate the value of both forms at index(0)

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,27 @@
 //Back-End Logic
 function Bank() {
   this.accounts = [];
+  this.accountId = 0;
 }
+
+Bank.prototype.addAccount = function(account) {
+  account.id = (this.accountId += 1);
+  this.accounts.push(account);
+};
 
 function Account(name, amount) {
   this.name = name;
   this.amount = amount;
+  
 }
 
+Account.prototype.updateAmount = function(deposit, withdrawal) {
+  this.deposit = deposit;
+  this.withdrawal = withdrawal;
+  this.amount = (this.amount + this.deposit - this.withdrawal);
+};
+
+var bank = new Bank();
 
 //Front-End Logic
 function showBalance(account) {
@@ -20,7 +34,18 @@ $("document").ready(function() {
     var inputtedName = $("input#name").val();
     var inputtedAmount = parseInt($("input#amount").val());
     var account = new Account(inputtedName, inputtedAmount);
+    bank.addAccount(account);
     showBalance(account);
+    
+    
+  });
+  $("form#update-balance").submit(function()  {
+    event.preventDefault();
+    var inputtedDeposit = parseInt($("#depositAmount").val());
+    var inputtedWithdrawal = parseInt($("#withdrawalAmount").val());
+    bank.accounts[0].updateAmount(inputtedDeposit, inputtedWithdrawal);
+    console.log(bank.accounts[0]);
+    showBalance(bank.accounts[0]);
 
   });
 
